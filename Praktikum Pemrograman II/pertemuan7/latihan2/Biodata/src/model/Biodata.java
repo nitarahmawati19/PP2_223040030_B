9e4b89a89faee29b457692067678dbd881160327
Nita Rahmawati <116252482+nitarahmawati19@users.noreply.github.com>
2024-11-11 15:16:07 +0700

    tugas pertemuan7

---
+++ Praktikum Pemrograman II/pertemuan7/latihan2/Biodata/src/model/Biodata.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Biodata {
+    private String id;
+    private String nama;
+    private String alamat;
+    private String telepon;
+
+    public Biodata(String id, String nama, String alamat, String telepon) {
+        this.id = id;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.telepon = telepon;
+    }
+
+    // Getter and Setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+}
